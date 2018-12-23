4c1d6ac5045a0f10ebf2dbd2a92f356a503ecdff
crassSandwich <christopherjanidlo@gmail.com>
2018-12-23 00:28:31 -0500

    Camera Movement

---
+++ Assets/Scripts/CameraRigRotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraRigRotator : MonoBehaviour
+{
+    public Button Left, Right;
+
+    void Start()
+    {
+        Left.onClick.AddListener(rotateLeft);
+        Right.onClick.AddListener(rotateRight);
+    }
+
+    void rotate (bool dirFlag)
+    {
+        var direction = dirFlag ? -1 : 1;
+        transform.Rotate(Vector3.up * direction * (45f / 4f));
+    }
+
+    void rotateLeft ()
+    {
+        rotate(true);
+    }
+
+    void rotateRight ()
+    {
+        rotate(false);
+    }
+}
