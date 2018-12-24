0e6ac6bba42fa80993ec289315868f0f0983493c
crassSandwich <christopherjanidlo@gmail.com>
2018-12-24 00:53:55 -0500

    Show register contents

---
+++ Assets/Scripts/UI/RegisterDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class RegisterDisplay : MonoBehaviour
+{
+    public string EmptyText;
+
+    Text text;
+    
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = "";
+        foreach (var action in Registers.Instance.Contents)
+        {
+            text.text += action.RegisterDisplayString + "\n";
+        }
+
+        if (text.text == "")
+        {
+            text.text = EmptyText;
+        }
+    }
+}
