c997b9e6e825ed9a8a7ac8ab3644779cc89f8a88
crassSandwich <christopherjanidlo@gmail.com>
2018-12-23 21:20:19 -0500

    Non-instantaneous rotation

--- Assets/Scripts/CameraRigRotator.cs
+++ Assets/Scripts/CameraRigRotator.cs
@@ -5,18 +5,27 @@ using UnityEngine.UI;
 
 public class CameraRigRotator : MonoBehaviour
 {
+    public float RotationSpeed;
     public Button Left, Right;
+
+    Quaternion targetRotation;
 
     void Start()
     {
         Left.onClick.AddListener(rotateLeft);
         Right.onClick.AddListener(rotateRight);
+        targetRotation = transform.rotation;
+    }
+
+    void Update ()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
     }
 
     void rotate (bool dirFlag)
     {
         var direction = dirFlag ? -1 : 1;
-        transform.Rotate(Vector3.up * direction * (45f / 4f));
+        targetRotation *= Quaternion.Euler(Vector3.up * direction * (45f / 4f));
     }
 
     void rotateLeft ()
